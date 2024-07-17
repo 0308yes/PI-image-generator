0f3431fe3b2b2e7827b24f78ecbe64e9525b94e4
{0308yes} <{0308yes@naver.com}>
2024-07-17 10:05:20 +0900

    Prompt update 0717

--- public/communication/openAI.js
+++ public/communication/openAI.js
@@ -2,20 +2,21 @@ export class OpenAI {
     constructor() {
         this.fetch = null
         this.promptGenerationRule = `
-            Image Generation Rules:
-            1. Extract keywords based on the interpretation of the data before generating the image. The image should be generated based on these keywords.
-            2. Do not include any numbers, letters, or text in the drawing.
-            3. Generate an image that is not easily associated with the data provided, ensuring creativity and originality.
-            4. When creating keywords, reflect the interpretation of the quantitative data rather than the type of data itself. For example, interpret "24-minute exercise" creatively instead of simply noting "exercise". The image should embody these keywords in a creative manner. Do not include numbers, letters, or text in the drawing. Do not draw objects that directly depict “physical activity” and “personal data”. Do not draw a road, trail, or path.
-            5. Upon regeneration, ensure the new image is based on different subjects or keywords.
-            6. The subject, mood, texture, and style of the image should vary with each generation to maintain diversity and creativity.
-            7. Draw in one of the following styles: 
+            [Image Generation Rules]
+            1. Extract keywords based on the interpretation of the data before generating the image. Keywords can include scenes, objects, landscapes, colors, patterns, moods, etc. The image should be generated based on these keywords.
+            2. When creating keywords, reflect the interpretation of the data rather than the type of data itself. For example, interpret "24-minute exercise" creatively instead of simply noting "exercise". The image should embody these keywords in a creative manner. Do not draw objects that directly depict the data type and “personal data” itself.
+            3. Do not include any numbers, letters, or text in the image.
+            4. Generate an image that is not easily associated with the data provided, ensuring creativity and originality.
+            5. Avoid repetitive image prompts by using diverse keywords and styles. The subject, mood, texture, and style of the image should vary to maintain diversity and creativity.
+            6. Draw in one of the following styles: 
             - Medium: painting, photo, sketch, cartoon, icon, vector, graffiti, 3D render
             - West Figurative Premodern: Baroque, High Renaissance, Impressionism, Medieval, Pointillism, Neoclassicism
             - West Figurative Modern: Pop Art, Surrealism, documentary photography, Art deco, Hippie movement, photorealism
             - Non-West Abstract Modern: Ukiyo-e, Chinese ink wash painting, Kerala mural, Mayan art, African masks, ancient Egyptian art, thangka
-            - Non-West Abstract Modern.: Mola art, Geometric Islamic art, Mexican Otomi, Andean textile, Aboriginal art
+            - Non-West Abstract Modern: Mola art, Geometric Islamic art, Mexican Otomi, Andean textile, Aboriginal art
             - West Abstract Modern: action painting, Op art, Bauhaus, Cubism, Dadaism, Futurism
+            7. Response should only be the image prompt, do not give any prefix such as “prompt”, “image prompt”, or “prompts for DALL-E” and do not use double quotes at the start and end of the response. 
+
 `
     }
 
@@ -25,7 +26,7 @@ export class OpenAI {
 
     defineMessage(data_types, data_category) {
         let data_type_keys = Object.keys(data_types);
-        let userMessageContent = `You will be provided with the personal data related to ${data_category} of a day, including "`;
+        let userMessageContent = `${data_category} of a day: `;
 
         // data_type_keys.forEach((key, index) => {
         //     userMessageContent += `${key}:${data_types[key]}`;
@@ -56,7 +57,7 @@ export class OpenAI {
         const message = [{
             role: "system",
             //content: 'You are a helpful assistant who creates image prompts. Your goal is to support self-reflection by creating an image that reflects the interpretation of personal data. You will be provided with the user’s ${data_category} data to inspire an image. Your task is to create a creative and original image prompt for DALL-E to produce an image that is inspired by your interpretation of this data. The output should only be the image prompt.',
-            content: `You are a helpful assistant who creates image prompts. Your goal is to support self-reflection by creating an image that reflects the interpretation of personal data. Given the user's ${data_category} data, you will be creating a creative and original image prompt for DALL-E to produce an image that is inspired by your interpretation of this data. The output should only be the image prompt.`,
+            content: `You are an assistant who creates image prompts. Your goal is to support self-reflection by creating an image that reflects the interpretation of personal data. You will be provided with the personal data related to “${data_category} of a day”. Given the user's ${data_category} data, your task is to create a creative and original image prompt for DALL-E to produce an image that is inspired by your interpretation of this data. You MUST follow the provided [Image Generation Rules] when generating image prompts. Provide your output in an image prompt format.`,
         },
         {
             role: "user",
@@ -90,7 +91,7 @@ export class OpenAI {
 
         const message = [{
             role: "system",
-            content: `You are a helpful assistant who creates image prompts. Your goal is to support self-reflection by creating an image that reflects the interpretation of personal data. Given the user's weekly ${data_category} data, you will be creating a creative and original image prompt for DALL-E to produce an image that is inspired by your interpretation of this weekly data. The output should only be the image prompt.`
+            content: `You are an assistant who creates image prompts. Your goal is to support self-reflection by creating an image that reflects the interpretation of personal data. You will be provided with the personal data related to “Weekly ${data_category} data”. Given the user's weekly ${data_category} data, your task is to create a creative and original image prompt for DALL-E to produce an image that is inspired by your interpretation of this data. You MUST follow the provided [Image Generation Rules] when generating image prompts. Provide your output in an image prompt format.`
         },
         {
             role: "user",
